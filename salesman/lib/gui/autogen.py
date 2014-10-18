dd343e61f9593dd1cb0ea75689ffb3fbe287eede
Miheer Dewaskar <miheerdew@gmail.com>
2014-10-18 18:00:50 +0530

    Upgraded wxformbuilder file to a new version

--- salesman/lib/gui/autogen.py
+++ salesman/lib/gui/autogen.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 ###########################################################################
-## Python code generated with wxFormBuilder (version Oct  8 2012)
+## Python code generated with wxFormBuilder (version Jun  6 2014)
 ## http://www.wxformbuilder.org/
 ##
 ## PLEASE DO "NOT" EDIT THIS FILE!
